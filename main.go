9749a47af0ac8d816597b05eb9574c8c286f2692
Aymane Chent <achent@noreply.learn.zone01oujda.ma>
2026-04-15 12:09:08 +0000

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 	"log"
 	"net/http"
 
+	"forum/internals/auth"
 	"forum/internals/database"
+	"forum/internals/forum"
+	"forum/internals/handlers"
+	"forum/internals/middleware"
 )
 
 func main() {
@@ -14,6 +18,35 @@ func main() {
 		log.Fatalf("Failed to initialize database: %v", err)
 	}
 	defer database.DB.Close()
+
+	// Create a new ServeMux
+
+	// Static Files
+	fs := http.FileServer(http.Dir("web/static"))
+	http.Handle("GET /static/", http.StripPrefix("/static/", fs))
+
+	//  Public Page Handlers
+	http.HandleFunc("/", middleware.Method("GET", handlers.HomeHandler))
+	http.HandleFunc("/page/{page}", middleware.Method("GET", handlers.HomeHandler))
+	http.HandleFunc("/post/{post_id}", middleware.Method("GET", handlers.PostDetails))
+	http.HandleFunc("/login", middleware.Method("GET", middleware.Guest(handlers.LoginPageHandler)))
+	http.HandleFunc("/register", middleware.Method("GET", middleware.Guest(handlers.RegisterPageHandler)))
+
+	//  Protected Page Handlers
+	http.HandleFunc("/create-post", middleware.Method("GET", middleware.AuthMiddleware(handlers.CreatePostPageHandler)))
+
+	//  Auth API (always public)
+	http.HandleFunc("/auth/register", middleware.Method("POST", middleware.Guest(auth.RegisterHandler)))
+	http.HandleFunc("/auth/login", middleware.Method("POST", middleware.Guest(auth.LoginHandler)))
+	http.HandleFunc("/auth/logout", middleware.Method("POST", middleware.AuthMiddleware(auth.LogoutHandler)))
+
+	//  Protected API Handlers
+	http.HandleFunc("/api/posts/create", middleware.Method("POST", middleware.AuthMiddleware(forum.CreatePost)))
+	http.HandleFunc("/api/posts/delete", middleware.Method("POST", middleware.AuthMiddleware(forum.DeletePost)))
+	http.HandleFunc("/api/comments", middleware.Method("POST", middleware.AuthMiddleware(forum.CreateCommentHandler)))
+	http.HandleFunc("/api/comments/delete", middleware.Method("POST", middleware.AuthMiddleware(forum.DeleteCommentHandler)))
+
+	http.HandleFunc("/api/likes", middleware.Method("POST", middleware.AuthMiddleware(forum.ReactionsHandler)))
 
 	fmt.Println("Server started on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
