007b93901756134269d4a7e0346f5d4ed5437593
achent <chentaymane234@gmail.com>
2026-04-15 13:07:08 +0100

    add middleware

---
+++ internals/middleware/middleware.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"forum/internals/auth"
+	"forum/internals/errors"
+	"net/http"
+)
+
+func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Check if user exists in session/cookie
+		if !auth.LoggedIn(r) {
+			errors.RenderError(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		// User is logged in → continue
+		next(w, r)
+	}
+}
+
+func Guest(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if auth.LoggedIn(r) {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+		// User is NOT logged in → continue
+		next(w, r)
+	}
+}
+
+func Method(method string, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != method {
+			// TODO RENDER ERROR
+			errors.RenderError(w, http.StatusText(405), 405)
+			return
+		}
+		next(w, r)
+	}
+}
